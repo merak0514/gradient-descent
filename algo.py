bb37dd171bac4de21a14881e515822b9d7bcefe2
panhaoxuan <haoxuanpan0514@outlook.com>
2022-01-05 14:21:51 +0800

    初步完成，但是是否要对数据做归一化呢？

--- algo.py
+++ algo.py
@@ -4,7 +4,7 @@ from numpy import ndarray as nd
 
 class GD:
 
-    def __init__(self, n, m, A: nd, b: nd, x_gt: nd, stopping_gap: nd = 0.1):
+    def __init__(self, n, m, A: nd, b: nd, x_gt: nd, stopping_gap: float = 0.1):
         self.n = n
         self.m = m  # 200 个数据
         self.A: nd = A
@@ -13,12 +13,17 @@ class GD:
         self.x: nd = self.starting_point()
         self.last_x: nd = self.x
 
-        self.beta = 0.8  # param for backtracking
+        # params
+        self.beta = 0.3  # param for backtracking
         self.alpha = 0.1  # param for backtracking
         self.stopping_gap = stopping_gap
 
+        # stats
+        self.stat_improves = []
+        self.stat_real_gap = []
+
     def starting_point(self) -> nd:
-        return np.random.normal(0, 1, self.n)
+        return np.zeros(self.n)
 
     @staticmethod
     def compute_norm(a: nd, b: nd) -> nd:
@@ -26,11 +31,11 @@ class GD:
 
     def f(self, x) -> nd:
         """原函数"""
-        return np.linalg.norm(np.matmul(self.A, self.x)-self.b) ** 2
+        return np.linalg.norm(np.matmul(self.A, x)-self.b) ** 2
 
     def f_der(self, x: nd) -> nd:
         """导函数（除以2之后）"""
-        return np.matmul(self.A.T, self.A, x) - np.matmul(self.A.T, self.b)
+        return np.matmul(np.matmul(self.A.T, self.A), x) - np.matmul(self.A.T, self.b)
 
     def backtracking(self, delta_x: nd) -> float:
         """
@@ -38,8 +43,12 @@ class GD:
         :param delta_x: 梯度方向
         """
         t: float = 1
-        while self.f(self.x+t*delta_x) - (self.f(self.x) + self.alpha * t * np.dot(self.f_der(self.x), delta_x)) >= 0:
+        judge = self.f(self.x+t*delta_x) - (
+                self.f(self.x) + self.alpha * t * np.dot(self.f_der(self.x), delta_x))
+        while judge >= 0:
             t = self.beta * t
+            judge = self.f(self.x + t * delta_x) - (
+                        self.f(self.x) + self.alpha * t * np.dot(self.f_der(self.x), delta_x))
         return t
 
     def update(self, t:float, delta_x: nd):
@@ -51,10 +60,14 @@ class GD:
 
     def gd(self) -> nd:
         self.x = self.starting_point()
-        self.last_x = self.x
-        while self.norm(self.x, self.last_x) > self.stopping_gap:
-            self.last_x = x
+        improve = 100
+        while improve > self.stopping_gap:
+            self.last_x = self.x
             _der = self.f_der(self.x)  # determine the descent direction
             _t = self.backtracking(_der)  # compute the step size
-            self.update(_t, delta_x)
+            self.update(_t, _der)
+            improve = self.compute_norm(self.x, self.last_x)
+            self.stat_improves.append(improve)
+            self.stat_real_gap.append(self.compute_norm(self.x, self.x_gt))
         return self.x
+
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from numpy.random import seed
 from data_generator import *
+from algo import GD
+seed(777)
 
+n = 50
+m = 200  # 200 个数据
+stopping_gap = 0.1  # 停止条件
 
 if __name__ == '__main__':
-    x_gt = gen_data_x()
-    A = gen_data_A()
+    x_gt = gen_data_x(n)
+    A = gen_data_A(m, n)
     _b_real = np.matmul(A, x_gt)
     var_Ax = np.var(_b_real)
     var_eps = var_Ax / 20
-    eps = gen_data_eps(np.sqrt(var_eps))
-    b_got = _b_real + eps
-
-    c = 0
+    eps = gen_data_eps(np.sqrt(var_eps), m)
+    b = _b_real + eps
+    print(f"相关性为{compute_corr(_b_real, b)}")
+    gd = GD(n=n, m=m, A=A, b=b, x_gt=x_gt, stopping_gap=stopping_gap)
+    x_est = gd.gd()
